38727c07d51e4f142af61fdb13f52ee0d85915d1
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-03 12:22:10 -0700

    Server Updated

--- app/data/friend.js
+++ app/data/friend.js
@@ -15,19 +15,19 @@ var matches =
         1
         ]
     },{
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Roberto",
+        "photo":"https://www.facebook.com/photo.php?fbid=10160408443525627&l=2dbc4937f7",
         "scores":[
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
             ]
 }]
 
